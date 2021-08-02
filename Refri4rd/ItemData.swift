d5071d85282c1acb28fbd693fb8ea164d92d7bcc
simyoju <dawnofspring@daum.net>
2021-08-02 23:32:59 +0900

    Initial Commit

---
+++ Refri4rd/ItemData.swift
@@ -0,0 +1,19 @@
+//
+//  ItemData.swift
+//  Refri4rd
+//
+//  Created by simyo on 2021/08/02.
+//
+
+import UIKit
+
+class ItemData {
+    var inventoryId : Int?
+    var typeId : Int?
+    var category : Int?
+    var count : Int?
+    var title : String?
+    var description : String?
+    var createAt : Date?
+    var updateAt : Date?
+}
