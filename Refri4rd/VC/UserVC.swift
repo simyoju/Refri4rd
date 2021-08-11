c389bb7f15f4e85bb3a90197eaae6aa415632b19
simyoju <dawnofspring@daum.net>
2021-08-11 23:45:22 +0900

    create user, login VC & storyboard

---
+++ Refri4rd/VC/UserVC.swift
@@ -0,0 +1,21 @@
+//
+//  UserVC.swift
+//  Refri4rd
+//
+//  Created by simyo on 2021/08/11.
+//
+
+import UIKit
+
+class UserVC: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+  
+
+}
